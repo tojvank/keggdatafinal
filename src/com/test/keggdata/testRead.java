f9d660eb47ce4135bfd39f75f87cc5078e4b6b25
bd-mahfuz <bt23.mahfuz@gmail.com>
2022-10-06 10:54:07 +0600

    creating hsa_adj file

--- src/com/test/keggdata/testRead.java
+++ src/com/test/keggdata/testRead.java
@@ -1,19 +1,21 @@
 package com.test.keggdata;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 //  questo file contiene il parsing fatto
 public class testRead {
+
+
+
     public static ArrayList<String> IsEnzymeReader() throws IOException {
         boolean movetoNextFile = false;
         ArrayList<String> IsEnzyme = new ArrayList<>();
@@ -123,13 +125,16 @@ public class testRead {
         //System.out.println("Enzimi con reactions: " + enzymes + " con dimensione: " + enzymes.size());
 
         System.out.println("totale enzimi" + IEnzyme.size());
+
     }
 
 
     public static ArrayList<String> enzymeReader() throws IOException {
         boolean movetoNextFile = false;
         ArrayList<String> enzymeList = new ArrayList<>();
-        ArrayList<String> reactionList = new ArrayList<>();
+
+
+
 
 
 
@@ -159,11 +164,13 @@ public class testRead {
         int totfiles=0;
         int lastIndex = 0;
         for (File file : folder.listFiles()) {
+            System.out.println("executing file:"+ file.getName() +" ---------------------------------------------------");
             totfiles++;
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line = reader.readLine();
             List<String> pathWayNumbers = new ArrayList<>();
             List<String> pathWays = new ArrayList<>(); // list will contain index and pathway number ex: 0;01100 (which will save in hsa_nl file)
+            ArrayList<String> reactionList = new ArrayList<>();
             while(line!=null && !line.contains("GENES")) {
 
                 if(line.contains("EC")){
@@ -185,17 +192,16 @@ public class testRead {
                             System.out.println(pathWays);
                         }
                         if(line.contains("RN: ")){
-                            System.out.println("in reaction");
-
                             // aggiungo le reaction che trovo
                             reactionList.addAll(getReactionsFromLine(line)); //togliere .\k0\    .txt
                             // saving pathway data to file (hsa_nl.txt)
-                            File hsaNlFile = getHsaNlFile();
+                            File hsaNlFile = getHsaNlFile("hsa_nl");
                             Path path = Paths.get(hsaNlFile.toURI());
                             Files.write(path, pathWays, StandardOpenOption.APPEND);
-                            //System.out.println(pathWayLine);
+
 
                             //chiamata rest api su rn
+                            makeApiCall(reactionList, pathWayNumbers);
                         }
                         line=reader.readLine();
                     }
@@ -208,16 +214,112 @@ public class testRead {
         }
         System.out.println("Sono Enzimi: " + enzymeList.size() + " su totale di " + totfiles); // 1819 su 1915
 
-        System.out.println("Sono Enzimi con reactions: " + reactionList.size() );
+
         // IsEnzymeGlobal = IsEnzyme;
-        return reactionList;
+        return enzymeList;
+    }
+
+    private static void makeApiCall(List<String> reactionList, List<String> pathWayNumbers) throws IOException {
+        List<String> repeatedCompoundList = new ArrayList<>();
+        Set<String> uniqueCompoundSet = new HashSet<>();
+
+        List<List<String>> repList = new ArrayList<>();
+
+        Set<String> matchedPathWayList = new HashSet<>();
+
+        int counter = 0;
+        for (String elem : reactionList) {
+            String urlCreated = "https://rest.kegg.jp/get/" + elem;
+            URL url = new URL(urlCreated);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            int responseCode = con.getResponseCode();
+            counter++;
+            System.out.println("GET Response Code :: " + responseCode + "counter: " + counter);
+
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+
+                    // preparing repeated compound list and unique compound set
+                    if (inputLine.contains("EQUATION")) {
+                        List<String> compoundList = Arrays.stream(inputLine.split(" "))
+                                .filter(s -> s.startsWith("C"))
+                                .collect(Collectors.toList());
+                        repeatedCompoundList.addAll(compoundList);
+                        uniqueCompoundSet.addAll(compoundList);
+                        repList.add(compoundList);
+                    }
+
+                    if (inputLine.contains("PATHWAY")) {
+                        String reactionPathWayNumber = getNumberFromReactionPathWay(inputLine);
+                        if (pathWayNumbers.contains(reactionPathWayNumber)) {
+                            // we found a node
+                            matchedPathWayList.add(reactionPathWayNumber);
+                            System.out.println("matched->"+ reactionPathWayNumber);
+                        }
+
+                    }
+                }
+
+                in.close();
+            } else {
+                System.out.println("GET request not worked");
+            }
+        }
+
+//        System.out.println(repeatedCompoundList);
+//        System.out.println(uniqueCompoundSet);
+
+        System.out.println(repList);
+
+
+
+        String outputStr = String.join(";", matchedPathWayList) + ";"+ repeatCount(repList);
+
+        System.out.println(outputStr+"--------------------------------------");
+
+        File hsaNlFile = getHsaNlFile("hsa_adj");
+        Path path = Paths.get(hsaNlFile.toURI());
+        Files.write(path, List.of(outputStr), StandardOpenOption.APPEND);
+
+    }
+
+    private static int repeatCount(List<List<String>> repList) {
+        // counting common in each compound with a map ex map.put("C00001", 3)
+        Map<String, Integer> foundCountMap = new HashMap<>();
+        Set<String> result = new HashSet<>(repList.get(0));
+        result.forEach(s -> foundCountMap.put(s, 1));
+        for (int i = 1; i < repList.size(); i++) {
+            for (String s: result) {
+                if (new HashSet<>(repList.get(i)).contains(s)) {
+                    foundCountMap.put(s, foundCountMap.get(s) + 1);
+                }
+            }
+        }
+
+        AtomicInteger commonCount = new AtomicInteger();
+        foundCountMap.forEach((compound, repeatNo) -> {
+            if (repeatNo == repList.size()) {
+                commonCount.getAndIncrement();
+            }
+        });
+        return commonCount.get();
     }
 
     private static String getNumberFromPathWay(String pathWayLine) {
         int mapIndex = pathWayLine.indexOf("map");
         String ss = pathWayLine.substring(mapIndex, pathWayLine.indexOf(" ", mapIndex)).strip();
-        String numberStr = ss.replaceFirst("map", "");
-        return numberStr;
+        return ss.replaceFirst("map", "");
+    }
+
+    private static String getNumberFromReactionPathWay(String pathWayLine) {
+        int mapIndex = pathWayLine.indexOf("rn");
+        String ss = pathWayLine.substring(mapIndex, pathWayLine.indexOf(" ", mapIndex)).strip();
+        return ss.replaceFirst("rn", "");
     }
 
     public static List<String> getReactionsFromLine(String line) {
@@ -225,14 +327,10 @@ public class testRead {
         return Arrays.asList(reactionStr.split(" "));
     }
 
-    public static File getHsaNlFile() throws IOException {
-        File file = new File("./output/hsa_nl.txt");
+    public static File getHsaNlFile(String fileName) throws IOException {
+        File file = new File("./output/"+fileName+".txt");
         if (!file.exists()) {
             file.createNewFile();
-        } else {
-            // clear the file while run the application
-            System.out.println("clearing hsl_nl file ........");
-            Files.newBufferedWriter(Paths.get(file.toURI()) , StandardOpenOption.TRUNCATE_EXISTING);
         }
         return file;
     }
