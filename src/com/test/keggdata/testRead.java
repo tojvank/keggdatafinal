35c402d394c316691c2c72f7bac41276303be86a
bd-mahfuz <bt23.mahfuz@gmail.com>
2022-10-06 08:22:32 +0600

    working with test read file

--- src/com/test/keggdata/testRead.java
+++ src/com/test/keggdata/testRead.java
@@ -1,9 +1,15 @@
 package com.test.keggdata;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //  questo file contiene il parsing fatto
@@ -123,7 +129,7 @@ public class testRead {
     public static ArrayList<String> enzymeReader() throws IOException {
         boolean movetoNextFile = false;
         ArrayList<String> enzymeList = new ArrayList<>();
-        ArrayList<String> IsEnzymeWithReaction = new ArrayList<>();
+        ArrayList<String> reactionList = new ArrayList<>();
 
 
 
@@ -151,22 +157,45 @@ public class testRead {
 
 
         int totfiles=0;
-        for (File file : folder.listFiles()) { //lettura singoli file
+        int lastIndex = 0;
+        for (File file : folder.listFiles()) {
             totfiles++;
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line = reader.readLine();
+            List<String> pathWayNumbers = new ArrayList<>();
+            List<String> pathWays = new ArrayList<>(); // list will contain index and pathway number ex: 0;01100 (which will save in hsa_nl file)
             while(line!=null && !line.contains("GENES")) {
+
                 if(line.contains("EC")){
+
                     enzymeList.add(file.getName()); //togliere .\k0\    .txt
                     // salvataggio delle varie cose per k0
                     line=reader.readLine();
-                    while(line!=null){
-                        if(line.contains("DBLINKS")){
-                            if(line.contains("RN: ")){
-                                // aggiungo le reaction che trovo
-                                IsEnzymeWithReaction.add(file.getName()); //togliere .\k0\    .txt
-                                //chiamata rest api su rn
+                    String pathWayLine = "";
+                    while(line!=null && !line.contains("GENES")){
+                        if (line.contains("PATHWAY")) {
+
+                            // preparing pathway number
+                            while (line != null && line.contains("map")) {
+                                pathWayNumbers.add(getNumberFromPathWay(line));
+                                pathWays.add(lastIndex+";"+ getNumberFromPathWay(line));
+                                line=reader.readLine();
+                                lastIndex ++;
                             }
+                            System.out.println(pathWays);
+                        }
+                        if(line.contains("RN: ")){
+                            System.out.println("in reaction");
+
+                            // aggiungo le reaction che trovo
+                            reactionList.addAll(getReactionsFromLine(line)); //togliere .\k0\    .txt
+                            // saving pathway data to file (hsa_nl.txt)
+                            File hsaNlFile = getHsaNlFile();
+                            Path path = Paths.get(hsaNlFile.toURI());
+                            Files.write(path, pathWays, StandardOpenOption.APPEND);
+                            //System.out.println(pathWayLine);
+
+                            //chiamata rest api su rn
                         }
                         line=reader.readLine();
                     }
@@ -179,9 +208,32 @@ public class testRead {
         }
         System.out.println("Sono Enzimi: " + enzymeList.size() + " su totale di " + totfiles); // 1819 su 1915
 
-        System.out.println("Sono Enzimi con reactions: " + IsEnzymeWithReaction.size() );
+        System.out.println("Sono Enzimi con reactions: " + reactionList.size() );
         // IsEnzymeGlobal = IsEnzyme;
-        return IsEnzymeWithReaction;
+        return reactionList;
     }
 
+    private static String getNumberFromPathWay(String pathWayLine) {
+        int mapIndex = pathWayLine.indexOf("map");
+        String ss = pathWayLine.substring(mapIndex, pathWayLine.indexOf(" ", mapIndex)).strip();
+        String numberStr = ss.replaceFirst("map", "");
+        return numberStr;
+    }
+
+    public static List<String> getReactionsFromLine(String line) {
+        String reactionStr = line.substring(line.lastIndexOf("RN:") + 3).strip();
+        return Arrays.asList(reactionStr.split(" "));
+    }
+
+    public static File getHsaNlFile() throws IOException {
+        File file = new File("./output/hsa_nl.txt");
+        if (!file.exists()) {
+            file.createNewFile();
+        } else {
+            // clear the file while run the application
+            System.out.println("clearing hsl_nl file ........");
+            Files.newBufferedWriter(Paths.get(file.toURI()) , StandardOpenOption.TRUNCATE_EXISTING);
+        }
+        return file;
+    }
 }
